9b985a075f76f4da3e5a2596b46faab7173ff38a
Ast3risk-ops <spacedude609@pm.me>
2025-02-01 10:18:53 -0500

    random subtitles

--- content/_index.md
+++ content/_index.md
@@ -2,11 +2,13 @@
 title = "The Caltrop Network"
 +++
 <script defer src="funny.js"></script>
+<script defer src="subtitle.js"></script>
 <img align="center" src="Caltrop_tfc.png#transparent#no-hover" width="200" alt="Caltrop Network logo, a caltrop grenade from TFC">
 <h1 align="center">The Caltrop Network</h1>
-<div align="center" style="font-size: 1.5rem;color: #a2a8b6;">
+<div id="subtitle" align="center" style="font-size: 1.5rem;color: #a2a8b6;"></div>
+<noscript><div id="subtitle" align="center" style="font-size: 1.5rem;color: #a2a8b6;">
 Hosting simple, yet fun servers.
-</div>
+</div></noscript>
 <br>
 <noscript>You need Javascript enabled to see a random funny image! You can look at a list of them <a href="https://github.com/CaltropNetwork/website/tree/main/content/funny" target="_blank" rel="noopener noreferrer">here</a> instead.</noscript>
 <div id=imageContainer></div>
---
+++ static/subtitle.js
@@ -0,0 +1,29 @@
+const messages = [
+    'Hosting simple, yet fun servers.',
+    'Now with salt!',
+    'hello vro',
+    'HEAVY IS MAGICAL WISH-GRANTING FAIRY',
+    'Damn, this stuff is addictive.',
+    '#DeathToUncletopia',
+    'I think skial is a bitcoin miner, my PC caught fire when I was trying to play 24/7 turbine.',
+    'Get bonked.',
+    '<i>For gamers, by gamers.</i>',
+    'Contains trace amounts of gamers.',
+    'pf2 1.0 when',
+    'One or more of your party members are French. You shall be penalized.',
+    'Did you know these are random?',
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    'Served at 3000 Kelvin',
+    'bottom text',
+    'urgh',
+    'And everyone died.',
+    'HOW?????',
+    'That stupid sentry is still up!',
+    'web <b>d</b><sub>e</sub>si<i>g</i>n <code>is</code> <b>m</b>y p<u>a</u><q>s</q><sup>s</sup><i>i</i>on.',
+    'You know you can read all these on github and shit right?',
+    '<code>wacky_races_pf2</code> 👻'
+];
+
+const messageIndex = Math.floor(Math.random() * messages.length);
+
+document.getElementById('subtitle').innerHTML = `<p>${messages[messageIndex]}</p>`;
